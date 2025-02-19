cbc40a17d9040725ea0e500cc480f7c6d68002c9
scarface naser <scarface.naser@gmail.com>
2025-02-19 19:38:58 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="title">Naser Almurad is Here</h1>
+      <h2  className="title">My team</h2>
+      <p>Hello world , We are  here💀</p>
+    </>
+  );
+}
+export default App;
