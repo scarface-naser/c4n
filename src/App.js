f3b78e7ef67907a38139c774287a1cd3465c2349
scarface naser <scarface.naser@gmail.com>
2025-02-19 19:51:51 +0300

    new changes in app.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 function App() {
   return (
     <>
-      <h1 className="title">Naser Almurad is Here</h1>
+      <h1 className="title">Scarface is Here</h1>
       <h2  className="title">My team</h2>
       <p>Hello world , We are  here💀</p>
+      <P>Here is a new update in this file and we will upload it to the githubbs</P>
     </>
   );
 }
